a05be358ae3163810990808cbc57c45bb0e7044d
Полина Безруких <polybezrukhih@gmail.com>
2022-12-14 01:56:47 +0700

    Исправила по flake8

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -4,7 +4,7 @@ from django.views.generic.base import TemplateView
 class AboutAuthorView(TemplateView):
     template_name = 'about/author.html'
 # Описать класс AboutAuthorView для страницы about/author
- 
+
 
 class AboutTechView(TemplateView):
     template_name = 'about/tech.html'
